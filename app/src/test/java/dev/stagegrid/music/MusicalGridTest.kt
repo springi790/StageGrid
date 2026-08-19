7522138293a987de1c0c411b203af583952ee7e3
Angel Ricardo Elvira Torres <80663242+springi790@users.noreply.github.com>
2026-08-19 13:50:45 -0600

    Test next-bar section quantization

--- app/src/test/java/dev/stagegrid/music/MusicalGridTest.kt
+++ app/src/test/java/dev/stagegrid/music/MusicalGridTest.kt
@@ -24,6 +24,14 @@ class MusicalGridTest {
     }
 
     @Test
+    fun nextSectionJumpCanUseEndOfCurrentBar() {
+        val grid = MusicalGrid(120.0, "4/4", 100L)
+
+        assertEquals(2_100L, grid.endOfBarContaining(740L))
+        assertEquals(4_100L, grid.endOfBarContaining(2_100L))
+    }
+
+    @Test
     fun numeratorControlsBeatsPerBar() {
         val grid = MusicalGrid(60.0, "6/8", 0L)
 
