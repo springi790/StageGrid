df18bd9bc1d72f1c7ded3173057ed7a4b45bc1f4
Angel Ricardo Elvira Torres <80663242+springi790@users.noreply.github.com>
2026-08-20 14:26:03 -0600

    Version StageGrid as 0.4.0-alpha05

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.stagegrid"
         minSdk = 28
         targetSdk = 37
-        versionCode = 23
-        versionName = "0.3.0-alpha05"
+        versionCode = 28
+        versionName = "0.4.0-alpha05"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
