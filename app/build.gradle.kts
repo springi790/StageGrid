e556df53d698b370fa1fe535a7f5fe0e0d402fd1
Angel Ricardo Elvira Torres <80663242+springi790@users.noreply.github.com>
2026-08-21 07:17:32 -0600

    chore: initialize 0.7.0-alpha05

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.stagegrid"
         minSdk = 28
         targetSdk = 37
-        versionCode = 50
-        versionName = "0.6.0-alpha05.6"
+        versionCode = 51
+        versionName = "0.7.0-alpha05"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
