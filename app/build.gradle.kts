51686452972001b33f1820c2de311e6f21a86e67
Angel Ricardo Elvira Torres <80663242+springi790@users.noreply.github.com>
2026-08-21 00:39:24 -0600

    chore: bump version to 0.6.0-alpha05.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.stagegrid"
         minSdk = 28
         targetSdk = 37
-        versionCode = 48
-        versionName = "0.6.0-alpha05.4"
+        versionCode = 49
+        versionName = "0.6.0-alpha05.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
