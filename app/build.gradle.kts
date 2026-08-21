7ef2f554477b798cd06bc6770db3a9c3966d3528
Angel Ricardo Elvira Torres <80663242+springi790@users.noreply.github.com>
2026-08-20 22:21:12 -0600

    build: identify Native Guide sequencing hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.stagegrid"
         minSdk = 28
         targetSdk = 37
-        versionCode = 44
-        versionName = "0.6.0-alpha05"
+        versionCode = 45
+        versionName = "0.6.0-alpha05.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
