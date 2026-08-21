f3487f3181dcea158dc157d75362a15fcfc6acae
Angel Ricardo Elvira Torres <80663242+springi790@users.noreply.github.com>
2026-08-21 13:59:11 -0600

    chore: bump MIDI monitor build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.stagegrid"
         minSdk = 28
         targetSdk = 37
-        versionCode = 51
-        versionName = "0.7.0-alpha05"
+        versionCode = 52
+        versionName = "0.7.0-alpha05.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
