5606bf15fb38e0fa77caf7b32b55ca2d47039b84
Angel Ricardo Elvira Torres <80663242+springi790@users.noreply.github.com>
2026-08-20 18:51:15 -0600

    Bump StageGrid Visual Refresh alpha version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.stagegrid"
         minSdk = 28
         targetSdk = 37
-        versionCode = 36
-        versionName = "0.5.0-alpha05.3"
+        versionCode = 37
+        versionName = "0.5.1-alpha01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
