d3139b22f7e6567ee71f8d6cc49eab81a2fa568f
Angel Ricardo Elvira Torres <80663242+springi790@users.noreply.github.com>
2026-08-22 08:27:11 -0600

    chore: bump StageGrid to 0.7.0-alpha05.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.stagegrid"
         minSdk = 28
         targetSdk = 37
-        versionCode = 58
-        versionName = "0.7.0-alpha05.7"
+        versionCode = 59
+        versionName = "0.7.0-alpha05.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
